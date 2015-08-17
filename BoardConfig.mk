ae4869109c9db4261c277f992854dda19aeb4dd6
NoelMacwan <yulespencer@gmail.com>
2015-08-17 19:08:39 +0530

    mint: TWRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,5 +29,26 @@ TARGET_OTA_ASSERT_DEVICE := LT30p,mint
 
 BOARD_HARDWARE_CLASS := device/sony/mint/cmhw
 
+#TWRP flags
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/Userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "barrier=1,noauto_da_alloc"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/mint/recovery/graphics.c
+TW_BRIGHTNESS_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_1/brightness
+TW_MAX_BRIGHTNESS := 255
+
 # inherit from the proprietary version
 -include vendor/sony/mint/BoardConfigVendor.mk
